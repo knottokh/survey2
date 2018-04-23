0a63ecf167dd0eda86132b400baaaf6456d30b89
Knotto <one2treego@hotmail.com>
2018-04-23 16:52:14 +0000

    change font path and email production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+    config.action_mailer.default_url_options = { :host => 'https://survey2-knottoc9.c9users.io' }
+    config.action_mailer.delivery_method = :smtp
+    
+      config.action_mailer.smtp_settings = {
+          :address => "smtp.gmail.com",
+          :port => 587,
+          :domain => "gmail.com",
+          :user_name => "merm.cu@gmail.com",
+          :password => "merm11111",
+          :authentication => "plain",
+          :enable_starttls_auto => true
+      }
 end
