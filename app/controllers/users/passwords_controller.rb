94626e23c2f23e46ea829db6fc4225192b6714f0
Knotto <one2treego@hotmail.com>
2018-05-01 16:17:42 +0000

    disable email

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,33 +2,44 @@
 
 class Users::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
-  # def new
-  #   super
-  # end
+   def new
+     super
+   end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+   def create
+
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+      yield resource if block_given?
+      
+      if !resource.nil?
+        #reset_password_token = reset_password_token = Devise.token_generator.digest(resource, :reset_password_token, resource.reset_password_token)
+        redirect_to edit_password_url(resource, reset_password_token: resource.send_reset_password_instructions )
+        #flash[:dataresourse] = resource.send_reset_password_instructions+ "---"+resource.reset_password_token
+        #redirect_to new_user_password_path
+      else
+        respond_with(resource)
+      end
+   end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+   def edit
+     super
+   end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+   def update
+     super
+   end
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+   def after_resetting_password_path_for(resource)
+     super(resource)
+   end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+   def after_sending_reset_password_instructions_path_for(resource_name)
+     super(resource_name)
+   end
 end
